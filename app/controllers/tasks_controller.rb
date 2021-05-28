23b6a3d32648af7c64b9462ecb506be5b1f34d0d
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-05-28 19:38:45 +0800

    completed task model and controller tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,10 +13,6 @@ class TasksController < ApplicationController
     @task = @category.tasks.build
   end
 
-  def edit
-    @task = @category.tasks.find(params[:id])
-  end
-
   def create
     @task = @category.tasks.build(task_params)
 
@@ -26,6 +22,27 @@ class TasksController < ApplicationController
       render :new
     end
 
+  end
+
+  def edit
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def update
+    @task = @category.tasks.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to category_tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = @category.tasks.find(params[:id])
+    @task.destroy
+
+    redirect_to category_tasks_path
   end
 
   private
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -3,10 +3,43 @@ require "test_helper"
 class TasksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = Category.create(name: 'Test Category')
+    @task = @category.tasks.build(name: 'test task', description: 'test description', deadline: DateTime.now.tomorrow)
+    @task.save
   end
 
   test "should get index" do
     get category_tasks_path(@category)
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_task_path(@category)
+    assert_response :success
+  end
+
+  test "should create new task" do
+    post category_tasks_path(@category), params: { task: { name: "test task name", description: 'test task description' } }
+    assert_response :redirect
+  end
+
+  test "should show existing task" do
+    get category_task_path(@category, @task)
+    assert_response :success
+  end
+
+  test "should edit an existing task" do
+    get category_task_path(@category, @task)
+    assert_response :success
+
+    patch category_task_path(@category, @task), params: { task: { name: "edited", description: 'edited description' } }
+    assert_response :redirect
+  end
+
+  test "should delete an existing category" do
+    get category_task_path(@category, @task)
+    assert_response :success
+
+    delete category_task_path(@category, @task)
+    assert_response :redirect
   end
 end
