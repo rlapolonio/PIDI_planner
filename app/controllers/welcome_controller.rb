56bad274dfb709af3779fb789ded8bd2c073924a
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-06-05 11:20:06 +0800

    completed tentative design for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,21 @@ require 'faker'
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @categories = current_user.categories.all
+    @categories = current_user.categories.all.limit(11)
+    @categories2 = current_user.categories.all
     @category_id_list = @categories.map { |cat| cat.id }.uniq
     @tasks = []
-    @list = Task.where(deadline: Date.today).or(Task.where(deadline: nil))
+    @list = Task.where(deadline: Date.today).or(Task.where(deadline: nil)).limit(7)
     @list.each do |t|
       if @category_id_list.include?(t.category_id)
         @tasks << t
+      end
+    end
+    @tasks2 = []
+    @list2 = Task.where(deadline: Date.today).or(Task.where(deadline: nil))
+    @list2.each do |t|
+      if @category_id_list.include?(t.category_id)
+        @tasks2 << t
       end
     end
   end
