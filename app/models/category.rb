f960bf1c2a3c2fe23582d58d468e2ddc727d76b6
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-06-08 19:27:18 +0800

    added scope to Category.name uniqueness validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks,  dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id,
+    message: "should only have one instance per user"}
 end
